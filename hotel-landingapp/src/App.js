834f93c2b0be9fc2ce39c4a1f0f569abea1bd632
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-02-14 16:21:35 +0000

    my introduction

--- hotel-landingapp/src/App.js
+++ hotel-landingapp/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is Ella I am here to workgit
+          This is Ella.  I am here to work
         </p>
         <a
           className="App-link"
